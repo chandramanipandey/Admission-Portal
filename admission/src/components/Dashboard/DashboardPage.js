b9c6c95ec95516b3c8357565221b62594cddf6d8
Shubham Bhavsar <shubhamcbhavsar198@gmail.com>
2022-04-12 23:26:12 +0530

    admin and student dashboard views

--- admission/src/components/Dashboard/DashboardPage.js
+++ admission/src/components/Dashboard/DashboardPage.js
@@ -1,13 +1,28 @@
 import React from "react"
-import { Card } from "react-bootstrap"
+import { Card, Row, Col } from "react-bootstrap"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "../CSS/DashboardPage.css"
 
 export function AdminDashboardPage() {
     return (
         <div>
-            {/* <div style={{ width: '300px', height: '180px', border: "1px solid black", borderRadius: "20px"}}></div> */}
-            <Card body style={{ width: '18rem' }}>Total Students</Card>
-            <Card body style={{ width: '18rem' }}>Student Fees</Card>
-            <Card body style={{ width: '18rem' }}>Fees Tracking</Card>
+            <Row>
+                <Col>
+                    <Card body><h1>Total Students 50</h1>
+                    <p></p><br />
+                    <a className="primary">See list of Students &gt;</a></Card>
+                </Col>
+                <Col>
+                    <Card body ><h1>Student Fees</h1>
+                    <p></p><br />
+                    <a className="primary">Check Fees &gt;</a></Card>
+                </Col>
+                <Col>
+                    <Card body ><h1>Fees Tracking</h1>
+                    <p></p><br />
+                    <a className="primary">Track Fees &gt;</a></Card>
+                </Col>
+            </Row>
         </div>
     )
 }
@@ -15,9 +30,32 @@ export function AdminDashboardPage() {
 export function StudentDashboardPage() {
     return (
         <div>
-            <Card body style={{ width: '18rem' }}>Register Yourself</Card>
-            <Card body style={{ width: '18rem' }}>Fees Corner</Card>
-            <Card body style={{ width: '18rem' }}>My Profile</Card>
+            <Row>
+                <Col>
+                    <Card body>
+                        <h1>Register Yourself</h1>
+                        <br></br>
+                        <br></br>
+                        <a href="#">Registration Form Link &gt;</a>
+                    </Card>
+                </Col>
+                <Col>
+                    <Card body>
+                        <h1>Fees Corner</h1>
+                        <br></br>
+                        <br></br>
+                        <a href="#">Fill Fee Details &gt;</a>
+                    </Card>
+                </Col>
+                <Col>
+                    <Card body>
+                        <h1>My Profile</h1>
+                        <br></br>
+                        <br></br>
+                        <a href="#">Check Profile &gt;</a>
+                    </Card>
+                </Col>
+            </Row>
         </div>
     )
 }
