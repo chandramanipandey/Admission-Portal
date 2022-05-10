6100cd88264a80d36489ee6d4b3e8773017dcb5e
Chandramani Pandey <royalchandramani@gmail.com>
2022-05-11 03:32:58 +0530

    Firebase Function addtransactionslip to firebase updated with new timestamp

--- admission/src/components/Firebase/addtransactionsliptofirebase.js
+++ admission/src/components/Firebase/addtransactionsliptofirebase.js
@@ -11,11 +11,18 @@ export async function addpaymentreceipt(uid, file, TransactionData) {
         const date = datevar.getDate().toString();
         const month = datevar.getMonth().toString();
         const year = datevar.getFullYear().toString();
-        const filename = date + '-' + month + '-' + year + '.jpg'
-        const keyname = uid + '-' + date + '-' + month + '-' + year;
+        const Hour = datevar.getHours().toString();
+        const Minutes = datevar.getMinutes().toString();
+        const Seconds = datevar.getSeconds().toString();
+        const keyname = uid + '-' + date + '|' + month + '|' + year + '-' + Hour + ':' + Minutes + ':' + Seconds;
+        const Timestamp = date + '|' + month + '|' + year + '-' + Hour + ':' + Minutes + ':' + Seconds;
+        const filename = Timestamp + '.jpg'
+        TransactionData['Timestamp'] = Timestamp
+
         const storageRef = ref(storage, `Fee_Receipts/${uid}/${filename}`);
         const uploadTask = await uploadBytes(storageRef, file);
         const downloadlink = await getDownloadURL(uploadTask.ref);
+
         delete TransactionData['transactionReceipt'];
         TransactionData['Receipt'] = downloadlink;
         // Add latest receipt links to Fee_receipts in rtdb
