8ec4a4156ccbecb299dbf4daaf689ff299651f1d
unknown <spriyanshu245@gmail.com>
2022-05-16 03:55:41 +0530

    Fee Structure changes

--- admission/src/components/RegistrationForms/Fields/NumField.js
+++ admission/src/components/RegistrationForms/Fields/NumField.js
@@ -4,9 +4,25 @@ import Form from "react-bootstrap/Form"
 import { useState, useContext } from "react"
 import { FieldsContext } from "../../States/FieldStates"
 
-export default function NumField({ title, maxlength, controlId, placeholder, isDisabled, size }) {
+export default function NumField({ title, maxlength, controlId, placeholder, isDisabled, size, value }) {
   
-  const { cetMeritNoState, parentsAnnualIncomeState, aadharNoState, senderAcNoState, guardianMobileState, studentMobileState } = useContext(FieldsContext)
+  const { 
+    cetMeritNoState, 
+    parentsAnnualIncomeState, 
+    aadharNoState, 
+    senderAcNoState, 
+    guardianMobileState, 
+    studentMobileState,
+    financialYearState,
+    openState,
+    omsState,
+    obc_ebcState,
+    scState,
+    st_ntState,
+    issueDateState,
+  } = useContext(FieldsContext)
+
+
 
   const [ cetMeritNo, setCetMeritNo ] = cetMeritNoState
   const [ parentsAnnualIncome, setParentsAnnualIncome ] = parentsAnnualIncomeState
@@ -14,6 +30,26 @@ export default function NumField({ title, maxlength, controlId, placeholder, isD
   const [ senderAcNo, setSenderAcNo ] = senderAcNoState
   const [ guardianMobile, setGuardianMobile] = guardianMobileState
   const [ studentMobile, setStudentMobile ] = studentMobileState
+
+  //Student categories
+  const [ financialYear, setFinancialYear ] = financialYearState
+  const [ open, setOpen ] = openState
+  const [ oms, setOms ] = omsState
+  const [ obc_ebc, setObc_ebc ] = obc_ebcState
+  const [ sc, setSc ] = scState
+  const [ st_nt, setSt_nt ] = st_ntState
+  const [ issueDate, setIssueDate ] = issueDateState
+
+  const [ inputsValues, setInputsValues ] = useState({
+    financialYear:financialYear,
+    open:open,
+    oms:oms,
+    obc_ebc:obc_ebc,
+    sc:sc,
+    st_nt:st_nt,
+    issueDate:issueDate,
+  });
+
   
   return (
     <div>
@@ -30,6 +66,7 @@ export default function NumField({ title, maxlength, controlId, placeholder, isD
               maxlength={maxlength}
               onChange={(e) => handleChange(e, controlId)}
               disabled
+              defaultValue = {value}
             />
             ) : (
               <Form.Control
@@ -37,6 +74,7 @@ export default function NumField({ title, maxlength, controlId, placeholder, isD
               placeholder={placeholder}
               maxlength={maxlength}
               onChange={(e) => handleChange(e, controlId)}
+              defaultValue = {value}
             />
             )}
           </Col>
@@ -46,10 +84,15 @@ export default function NumField({ title, maxlength, controlId, placeholder, isD
   )
 
   function handleChange(e, controlId) {
+    const name = e.target.name;
+    const value = e.target.value;
+    inputsValues[e.target.name] = e.target.value;
+    setInputsValues({...inputsValues, [name] : value });
+    console.log(inputsValues);
+
     switch (controlId) {
       case "cetMeritNo":
         setCetMeritNo(e.target.value)
-        console.log(cetMeritNo)
         break
       case "parentsAnnualIncome":
         setParentsAnnualIncome(e.target.value)
@@ -62,11 +105,30 @@ export default function NumField({ title, maxlength, controlId, placeholder, isD
         break
       case "guardianMobile":
         setGuardianMobile(e.target.value)
-        console.log(guardianMobile)
         break
       case "studentMobile":
         setStudentMobile(e.target.value)
-        console.log(studentMobile)
+        break
+      case "financialYear":
+        setFinancialYear(e.target.value)
+        break
+      case "open":
+        setOpen(e.target.value)
+        break
+      case "oms":
+        setOms(e.target.value)
+        break
+      case "obc_ebc":
+        setObc_ebc(e.target.value)
+        break
+      case "sc":
+        setSc(e.target.value)
+        break
+      case "setSt_nt":
+        setSt_nt(e.target.value)
+        break
+      case "setIssueDate":
+        setIssueDate(e.target.value)
         break
       default:
         console.log("Does not match any case")
