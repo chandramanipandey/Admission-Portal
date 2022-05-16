7b64af9703416fca80bd4e9dccac54cc23835117
Shubham Bhavsar <shubhamcbhavsar198@gmail.com>
2022-05-17 02:46:34 +0530

    notice Student View Complete

--- admission/src/components/Dashboard/DashboardPage.js
+++ admission/src/components/Dashboard/DashboardPage.js
@@ -1,7 +1,7 @@
-import React, { useEffect, useState } from "react"
-import { Card, Row, Col } from "react-bootstrap"
+import React, { useEffect, useState } from "react";
+import { Card, Row, Col } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import "../CSS/DashboardPage.css"
+import "../CSS/DashboardPage.css";
 import { useHistory, Link } from "react-router-dom";
 import { receivefromfirebase } from "../Firebase/receivefromfirebase";
 import { getAuth } from "firebase/auth";
@@ -9,36 +9,63 @@ import DashboardCard from "./DashboardCard";
 import { checknewuser } from "../Firebase/checknewstudent";
 
 export function AdminDashboardPage() {
-
-    return (
-        <div>
-            <Row>
-                <DashboardCard title="Total Students" titleText="See list of Students &gt;" controlId="StudentList" />
-                <DashboardCard title="Student Fees" titleText="Check Approved / Pending Fees &gt;" controlId="StudentFees" />
-                <DashboardCard title="Fees Tracking" titleText="Track Fees &gt;" controlId="fedseform" />
-            </Row>
-            <Row className="mt-3">
-                <DashboardCard title="Notice Generation" titleText="Generate Notice &gt;" controlId="GenerateNotice" />
-                <DashboardCard title="Fee Structure" titleText="Manage Fee Structure &gt;" controlId="FeeStructure" />
-            </Row>
-        </div>
-    )
+  return (
+    <div>
+      <Row>
+        <DashboardCard
+          title="Total Students"
+          titleText="See list of Students &gt;"
+          controlId="StudentList"
+        />
+        <DashboardCard
+          title="Student Fees"
+          titleText="Check Approved / Pending Fees &gt;"
+          controlId="StudentFees"
+        />
+        <DashboardCard
+          title="Fees Tracking"
+          titleText="Track Fees &gt;"
+          controlId="fedseform"
+        />
+      </Row>
+      <Row className="mt-3">
+        <DashboardCard
+          title="Notice Generation"
+          titleText="Generate Notice &gt;"
+          controlId="GenerateNotice"
+        />
+        <DashboardCard
+          title="Fee Structure"
+          titleText="Manage Fee Structure &gt;"
+          controlId="FeeStructure"
+        />
+      </Row>
+    </div>
+  );
 }
 
 export function StudentDashboardPage() {
-    const auth = getAuth();
+  const auth = getAuth();
 
-
-    return (
-        <div>
-            <Row className="mt-3">
-                <DashboardCard title="Register Yourself" titleText="Registration Form Link &gt;" controlId="fedseform" />
-                <DashboardCard title="Fees Corner" titleText="Fill Fee Details &gt;" controlId="FeesDetails" />
-                <DashboardCard title="My Profile" titleText="Check Profile &gt;" controlId="MyProfile" />
-                
-            </Row>
-
-        </div>
-
-    )
+  return (
+    <div>
+      <Row className="mt-3">
+        <DashboardCard
+          title="Register Yourself"
+          titleText="Registration Form Link &gt;"
+          controlId="fedseform"
+        />
+        <DashboardCard
+          title="Fees Corner"
+          titleText="Fill Fee Details &gt;"
+          controlId="FeesDetails"
+        />
+        <DashboardCard
+          title="My Profile"
+          titleText="Check Profile &gt;"
+          controlId="MyProfile"
+        />
+      </Row>
+    </div>
+  );
 }
